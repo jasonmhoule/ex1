e19a25a05b23431daacaf1ee81dd7b857db23500
jasonmhoule <jasonmhoule@gmail.com>
2020-05-02 10:54:18 +0000

    Include statement to load config files to generic script

--- config/files/gs_loader.R
+++ config/files/gs_loader.R
@@ -3,6 +3,9 @@ proj_name <- ""
 # Connect
 system("gcloud init --account=652580964228-compute@developer.gserviceaccount.com --project=ml-learning-199501", intern = TRUE)
 
+# Load config files
+system("gsutil cp -r gs://jmh_config/jmh_config/* .")
+
 # Download
 system(paste0("gsutil cp -r gs://jmh/", proj_name, " ."), intern = TRUE)
 
