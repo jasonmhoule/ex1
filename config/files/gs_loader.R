645042f83e4e3d3deafe71894e07263be694ae20
Jason Houle <jasonmhoule@gmail.com>
2020-05-04 06:27:20 -0500

    Update gs_loader.R

--- config/files/gs_loader.R
+++ config/files/gs_loader.R
@@ -3,11 +3,14 @@ proj_name <- ""
 # Connect
 system("gcloud init --account=652580964228-compute@developer.gserviceaccount.com --project=ml-learning-199501", intern = TRUE)
 
-# Load config files
-system("gsutil cp -r gs://jmh_config/jmh_config/* .")
+# Load config files and change RSA file permissions
+system("gsutil cp -r gs://jmh_config/jmh_config/* . && chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub")
 
 # Download
-system(paste0("gsutil cp -r gs://jmh/", proj_name, " ."), intern = TRUE)
+system(paste0("gsutil -m cp -r gs://jmh/", proj_name, " ."), intern = TRUE)
 
 # Upload
-system(paste0("gsutil cp -r . gs://jmh/", proj_name), intern = TRUE)
+system(paste0("gsutil -m cp -r . gs://jmh/", proj_name), intern = TRUE)
+
+# Once loaded and logged into project, one-time setup to change repo origin to use SSH
+system(paste0("git remote set-url origin git@github.com:jasonmhoule/", proj_name, ".git"), intern = TRUE)
