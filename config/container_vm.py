2f536cca8b72bf8a4d34dd6068c5d0ed83bd1c83
jasonmhoule <jasonmhoule@gmail.com>
2020-05-06 02:50:01 +0000

    test

--- config/container_vm.py
+++ config/container_vm.py
@@ -14,7 +14,7 @@
 
 """Creates a Container VM with the provided Container manifest."""
 
-# import manitest
+import manmaker from manitest
 
 COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
@@ -60,7 +60,7 @@ def GenerateConfig(context):
       restartPolicy: Always
   """.format(**context.properties)
   
-  # MANIFEST = manitest.manmaker(context)
+  MANIFEST = manmaker(context)
           
   # Properties for the container-based instance.
   instance = {
@@ -72,7 +72,7 @@ def GenerateConfig(context):
       'metadata': {
           'items': [{
               'key': 'gce-container-declaration',
-              'value': 'manitest.py'
+              'value': MANIFEST
               },{
               'key': 'google-logging-enabled',
               'value': 'true'
