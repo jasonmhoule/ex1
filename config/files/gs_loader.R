09db52a57af2efcfd8b7feb26903d2784c6eec0f
jasonmhoule <jasonmhoule@gmail.com>
2020-05-11 14:27:12 +0000

    Change SSH setup

--- config/files/gs_loader.R
+++ config/files/gs_loader.R
@@ -1,14 +1,19 @@
 proj_name <- ""
 
+## OUTSIDE Project
 # Initialize and clone repo (using SSH)
 system(paste0("gcloud init --account=652580964228-compute@developer.gserviceaccount.com --project=ml-learning-199501 && ",
               "gsutil cp -r gs://jmh_config/jmh_config/* . && ",
               "chmod 600 ~/.ssh/id_rsa && ",
               "chmod 600 ~/.ssh/id_rsa.pub && ",
-              "git clone git@github.com:jasonmhoule/", proj_name, ".git"))
+              "git clone https://github.com/jasonmhoule/", proj_name, ".git"))
 
-# From outside project, to download & overwrite files from bucket (assumes existing folder)
+# Download & overwrite files from bucket (assumes existing folder)
 system(paste0("gsutil -m cp -r gs://jmh/", proj_name, " ."))
 
-# From within project, resync files to bucket (creates folder if needed; ignores .git)
+## WITHIN Project
+# For setup - change repo to SSH
+system(paste0("git remote set-url origin git@github.com:jasonmhoule/", proj_name, ".git"), intern = TRUE)
+
+# Resync files to bucket (creates folder if needed; ignores .git)
 system(paste0("gsutil -m rsync -rd -x .git . gs://jmh/", proj_name))
