a4f6ae4521216c19a4d7022e83d9015ac5426b88
jasonmhoule <jasonmhoule@gmail.com>
2020-05-11 11:24:24 +0000

    Update GCS interface

--- config/files/gs_loader.R
+++ config/files/gs_loader.R
@@ -1,18 +1,14 @@
 proj_name <- ""
 
-# Connect
-system("gcloud init --account=652580964228-compute@developer.gserviceaccount.com --project=ml-learning-199501", intern = TRUE)
+# Initialize and clone repo (using SSH)
+system(paste0("gcloud init --account=652580964228-compute@developer.gserviceaccount.com --project=ml-learning-199501 && ",
+              "gsutil cp -r gs://jmh_config/jmh_config/* . && ",
+              "chmod 600 ~/.ssh/id_rsa && ",
+              "chmod 600 ~/.ssh/id_rsa.pub && ",
+              "git clone git@github.com:jasonmhoule/", proj_name, ".git"))
 
-# Load config files and change RSA file permissions
-system("gsutil cp -r gs://jmh_config/jmh_config/* . && chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub")
+# From outside project, to download & overwrite files from bucket (assumes existing folder)
+system(paste0("gsutil -m cp -r gs://jmh/", proj_name, " ."))
 
-# Download all project files from GCS
-system(paste0("gsutil -m cp -r gs://jmh/", proj_name, " ."), intern = TRUE)
-
-# Upload all project files to GCS
-system(paste0("gsutil -m cp -r . gs://jmh/", proj_name), intern = TRUE)
-
-# Once loaded and logged into project, one-time setup to change repo origin to use SSH
-system(paste0("git remote set-url origin git@github.com:jasonmhoule/", proj_name, ".git"), intern = TRUE)
-
-# one test commit
+# From within project, resync files to bucket (creates folder if needed; ignores .git)
+system(paste0("gsutil -m rsync -rd -x .git . gs://jmh/", proj_name))
